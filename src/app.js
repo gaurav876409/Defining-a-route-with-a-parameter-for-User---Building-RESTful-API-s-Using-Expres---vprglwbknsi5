1870684917713ff8c2a423f1941976e796454534
gaurav876409 <gaurav876409@gmail.com>
2023-07-17 09:17:43 +0530

    up

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const app = express();
 
 app.get('/user/:userId', (req, res) => {
   //Write a code here for endpoint /user/:userId and also print parameter in json form
-  res.status(200).json(req.params.userId)
+  const user = {
+    userId: req.params.userId,
+};
+  res.status(200).json(user)
 });
 
 module.exports = app;
